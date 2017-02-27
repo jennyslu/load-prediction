8be07554592be1b54d5afd7e158642cbcd443d95
Jenny Lu <ulynnej@gmail.com>
2017-02-27 15:25:03 -0800

    finished results

--- README.md
+++ README.md
@@ -24,19 +24,19 @@ After doing EDA, it doesn't seem that temperature is the most important feature 
 
 It seems like the most significant predictor for what the load will be is what the load was. However, depending on the situation, the lag to use here varies. For example, when trying to predict Saturday, the data from Friday is probably not as useful as the data from last Friday due to the reduced usage and different trends on weekdays vs weekends.
 
-Based on this, I think that having multiple models to predict various periods of time within the 24 hours would be the best approach. It would also be helpful to have separate weekday and weekend models. The most extreme - and potentially best - implementation of this would be to have 192 models: one for every 15 minute interval for the next 24 hours and weekend and weekday versions of each. However, given the time constraints here I will just build a few.
+Based on this, I think that having multiple models to predict various periods of time within the 24 hours would be the best approach. It would also be helpful to have separate weekday and weekend models. The most extreme - and potentially best - implementation of this would be to have at least 96 models: one for every 15 minute interval for the next 24 hours and weekend and weekday versions of each. However, given the time constraints here I will just build a few.
 
 From EDA, it was also clear that there are significant correlations between temperature and the time variables (time of day and month for instance).
 
-In order to incorporate the idea that both temperature and previous load are important features in predicting future load and also include the interactions between all these, I decided to try ensemble tree algorithms with lag variables rather than time series modelling as well as linear regression with dummy variables for seasonality.
-
-### Short-term model
-
-For this model I will just use a simple regression to project at most a few hours into the future using a few hours from the past.
+In order to incorporate the idea that both temperature and previous load are important features in predicting future load and also include the interactions between all these, I decided to try ensemble tree algorithms with lag variables rather than time series modelling or regression.
 
 ### Weekend model
 
-This model will be trained as a regression on last weekend
+This model will be trained as a regression on just past weekend data. Based on EDA it looks like weekend usage actually goes from around 4:00 on Saturdays through to around 8:00 on Mondays.
+
+### Weekday model
+
+One of the main disadvantages with using a RandomForestRegressor is that it will not be able to predict any value outside of the range of training data. However, it is useful in terms of being able to determine feature importance on its own as well as accounting for 'interactions' between features.
 
 ## Starting point
 
@@ -50,35 +50,125 @@ The question becomes: what data should be used for temperature in order to predi
 
 Ultimately, I decided to use future actual temperature data to test and validate. Given more time, I would instead hit a weather API and get forecasted temperature for the next 24 hours. The critical assumption here is that temperature forecasts are relatively accurate within a 24 hour period, i.e. forecasted temperature for the next 24 hours would not have differed that much from the actual temperature.
 
+## Re-training frequency
+
+I could retrain the models every 15 minutes as new data is added but for the purposes of this exercise I will retrain every 24 hours instead.
+
 
 # Validation
 
+All results are recorded in `out.log`
+
 ## Regression results
 
-Here's an example output of the regression results for the short-term forecaster model. p-value for temperature was not significant generally, and I think this confirms the idea that - at least in short-term - the most important predictor of load is simply past load.
+An example of regression results:
 
 ```
-                        OLS Regression Results
+Sat, Nov 17, 2012
+                            OLS Regression Results                            
 ==============================================================================
-Dep. Variable:                      y   R-squared:                       0.640
-Model:                            OLS   Adj. R-squared:                  0.520
-Method:                 Least Squares   F-statistic:                     5.337
-Date:                Wed, 22 Feb 2017   Prob (F-statistic):             0.0466
-Time:                        23:21:53   Log-Likelihood:                -12.358
-No. Observations:                   9   AIC:                             30.72
-Df Residuals:                       6   BIC:                             31.31
-Df Model:                           2
-Covariance Type:            nonrobust
+Dep. Variable:                      y   R-squared:                       0.740
+Model:                            OLS   Adj. R-squared:                  0.733
+Method:                 Least Squares   F-statistic:                     102.2
+Date:                Mon, 27 Feb 2017   Prob (F-statistic):           3.62e-60
+Time:                        14:36:15   Log-Likelihood:                -386.47
+No. Observations:                 222   AIC:                             786.9
+Df Residuals:                     215   BIC:                             810.8
+Df Model:                           6                                         
+Covariance Type:            nonrobust                                         
 ==============================================================================
-                coef    std err          t      P>|t|      [95.0% Conf. Int.]
+                 coef    std err          t      P>|t|      [95.0% Conf. Int.]
 ------------------------------------------------------------------------------
-const        202.0711    118.000      1.712      0.138       -86.665   490.807
-x1            -2.9455      2.330     -1.264      0.253        -8.647     2.756
-x2            -0.7250      0.262     -2.772      0.032        -1.365    -0.085
+const         48.5763      1.421     34.188      0.000        45.776    51.377
+x1            -0.3780      0.033    -11.568      0.000        -0.442    -0.314
+x2             0.0055      0.001      9.322      0.000         0.004     0.007
+x3         -3.171e-05   3.93e-06     -8.075      0.000     -3.95e-05  -2.4e-05
+x4          6.529e-08   8.69e-09      7.513      0.000      4.82e-08  8.24e-08
+x5             0.0634      0.029      2.209      0.028         0.007     0.120
+x6            -0.1068      0.016     -6.489      0.000        -0.139    -0.074
 ==============================================================================
-Omnibus:                        0.534   Durbin-Watson:                   1.851
-Prob(Omnibus):                  0.766   Jarque-Bera (JB):                0.147
-Skew:                           0.269   Prob(JB):                        0.929
-Kurtosis:                       2.679   Cond. No.                     1.53e+04
+Omnibus:                        4.221   Durbin-Watson:                   1.291
+Prob(Omnibus):                  0.121   Jarque-Bera (JB):                5.141
+Skew:                          -0.092   Prob(JB):                       0.0765
+Kurtosis:                       3.722   Cond. No.                     1.24e+10
 ==============================================================================
 ```
+
+Results varied quite dramatically. For example:
+
+```
+Sun, May 12, 2013
+                            OLS Regression Results                            
+==============================================================================
+Dep. Variable:                      y   R-squared:                       0.222
+Model:                            OLS   Adj. R-squared:                  0.207
+Method:                 Least Squares   F-statistic:                     14.78
+Date:                Mon, 27 Feb 2017   Prob (F-statistic):           7.22e-15
+Time:                        14:37:41   Log-Likelihood:                -748.05
+No. Observations:                 318   AIC:                             1510.
+Df Residuals:                     311   BIC:                             1536.
+Df Model:                           6                                         
+Covariance Type:            nonrobust                                         
+==============================================================================
+                 coef    std err          t      P>|t|      [95.0% Conf. Int.]
+------------------------------------------------------------------------------
+const         52.6459      1.596     32.978      0.000        49.505    55.787
+x1            -0.1512      0.034     -4.464      0.000        -0.218    -0.085
+x2             0.0015      0.000      3.445      0.001         0.001     0.002
+x3         -5.348e-06   2.02e-06     -2.644      0.009     -9.33e-06 -1.37e-06
+x4           6.24e-09   3.14e-09      1.986      0.048      5.68e-11  1.24e-08
+x5            -0.0268      0.026     -1.043      0.298        -0.077     0.024
+x6            -0.1012      0.025     -4.089      0.000        -0.150    -0.052
+==============================================================================
+Omnibus:                      364.399   Durbin-Watson:                   0.482
+Prob(Omnibus):                  0.000   Jarque-Bera (JB):            21340.907
+Skew:                           5.008   Prob(JB):                         0.00
+Kurtosis:                      41.863   Cond. No.                     3.80e+10
+==============================================================================
+```
+Given more time I would like to do
+
+## RandomForest results
+
+I looked at feature importances each time model was retrained. Top 5 values generally remain consistent, although order does change.
+
+```
+RandomForest Regression Results
+
+==============================================================================
+Feature importances:
+
+['lag-1',
+'last_week',
+'hour',
+'yesterday',
+'temp',
+'lag-11',
+'lag-2',
+'lag-3',
+'lag-7',
+'lag-4',
+'lag-6',
+'lag-9',
+'lag-10',
+'lag-5',
+'lag-8',
+'lag-12',
+'dow']
+```
+
+## Overall results
+
+![results](results.png)
+
+We can see that model does not too bad up until April 2013 but testing error increases significantly at this point.
+
+Overall, training error remains low for the entire dataset. 
+
+It appears that the model regularly fails to predict a certain time period accurately.
+
+![monthly_results](monthly_results.png)
+
+We can see from here that it is the transition zone from weekdays to weekends and vice versa that the model consistently gets high test error on. Some of the weekends themselves also have higher error, as predicted from wildly varying R-squared results. With a more sophisticated ensemble model I think I could capture the pattern of this transition better and produce a better model for weekend usage.
+
+As suspected, anomalies such as outage and irregular weekends did not get predicted accurately. In the scope of this exercise, this is to be expected.
--- src/Model.py
+++ src/Model.py
@@ -3,15 +3,19 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 import pandas as pd
+from pprint import pprint
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 import statsmodels.api as sm
+import sys
 
 
 class BaseModel(metaclass = ABCMeta):
     """BaseModel abstract class"""
     @abstractmethod
     def train(self):
-        '''train method should train model given data and current time'''
+        '''train method should train model given data and current time
+        also returns training error'''
         pass
 
     @abstractmethod
@@ -25,92 +29,85 @@ class BaseModel(metaclass = ABCMeta):
         '''score method should return errors for 96 future predictions'''
         pass
 
-class ShortTermModel(BaseModel):
-    """
-    Predict next x hours with simple linear regression based on past x hours
-    """
-    def __init__(self, x):
-        super().__init__()
-        self._x = x
-        self._mask = [1]*(x*4)
-        self._mask.extend([0]*(96-x*4))
-
-    def train(self, df, curr_time):
-        '''takes in dataframe with temperature and load and trains simple
-        linear regression of past x hours'''
-        self._curr = curr_time
-        # start of training period
-        self._start = curr_time - np.timedelta64(self._x, 'h')
-        # get training data
-        train = df.loc[self._start:curr_time]
-        # dummy time variable for linear regression
-        train['time'] = np.arange(1,train.shape[0]+1)
-        self._train = train
-        y_train = train.pop('load').values
-        X_train = train.values
-        # add constant
-        X_train = np.insert(X_train, 0, 1, axis=1)
-        # train model
-        self._model = sm.OLS(y_train, X_train)
-        self._result = model.fit()
-
-    def predict(self, temps):
-        # predict 2 hours into future
-        self._end = self._curr + np.timedelta64(24, 'h')
-        pred_range = pd.date_range(self._start, periods = 96, freq='15Min')
-        # get "forecasted" temperatures
-        test = temps[pred_range].toframe()
-        # dummy time variable
-        test['time'] = np.arange(1,test.shape[0]+1)
-        # start at next 15min interval
-        test_start = self._curr + np.timedelta64(15, 'm')
-        X_test = test.values
-        X_test = np.insert(X_test, 0, 1, axis=1)
-        # predict
-        test['predicted'] = self._result.predict(X_test)
-        self._test = test
-        return test.loc[test_start:,'load']
-
-    def score(self):
-        test['actual'] = df.loc[start:end, 'load']
-
 
 class WeekendModel(BaseModel):
     """
     Polynomial linear regression model for weekend
     To be used from 04:00 Saturday through to 08:00 Monday
     """
+    def __init__(self, p):
+        super().__init__()
+        self._p = p
+
+    def train(self, train):
+        '''given dataframe of training values, train linear model'''
+        y_train = train.pop('load').values
+        # dummy time variable for linear regression
+        timevars = ['time']
+        train['time'] = np.arange(1,train.shape[0]+1, dtype=np.int64)
+        for p in np.arange(2, self._p):
+            colname = 'time^{}'.format(p)
+            timevars.append(colname)
+            train[colname] = np.power(train['time'], p)
+        timevars.extend(['temp', 'hour'])
+        X_train = train.loc[:,timevars].values
+        # add constant
+        X_train = np.insert(X_train, 0, 1, axis=1)
+        self._model = sm.OLS(y_train, X_train)
+        self._results = self._model.fit()
+        print(self._results.summary())
+        return mean_squared_error(y_train, self._results.predict(X_train))**0.5
+
+    def predict(self, test):
+        '''given dataframe of test values, predict next 24 hours'''
+        timevars = ['time']
+        test['time'] = np.arange(1,test.shape[0]+1, dtype=np.int64)
+        for p in np.arange(2, self._p):
+            colname = 'time^{}'.format(p)
+            timevars.append(colname)
+            test[colname] = np.power(test['time'], p)
+        timevars.extend(['temp', 'hour'])
+        X_test = test.loc[:,timevars].values
+        # add constant
+        X_test = np.insert(X_test, 0, 1, axis=1)
+        self._y_pred = self._results.predict(X_test)
+        return self._y_pred
+
+    def score(self, y_test):
+        '''return RMSE of predictions'''
+        return mean_squared_error(y_test, self._y_pred)**0.5
+
+
+class WeekdayModel(BaseModel):
+    """
+    RandomForest regressor for weekdays
+    To be used from 8:15 Monday through 3:45 Saturday"""
     def __init__(self):
         super().__init__()
+        self._model = RandomForestRegressor()
 
-    def train(self, df, curr_time):
-        ''''train on last weekend'''
-        self._curr = curr_time
-        # start of training period
-        self._start = curr_time
+    def train(self, train):
+        '''given dataframe of training values, train RF regressor'''
+        y_train = train.pop('load').values
+        train.drop('wknd', axis=1, inplace=True)
+        X_train = train.values
+        self._model.fit(X_train, y_train)
+        print('''                            RandomForest Regression Results\n
+==============================================================================
+Feature importances: \n''')
+        pprint(train.columns[np.argsort(self._model.feature_importances_)[::-1]].tolist())
+        print("----------------------------\n")
+        return mean_squared_error(y_train, self._model.predict(X_train))**0.5
 
+    def predict(self, test):
+        '''given dataframe of test values, predict next 24 hours'''
+        X_test = test.values
+        self._y_pred = self._model.predict(X_test)
+        return self._y_pred
 
-class LongRangeModel(BaseModel):
-    """
-    Predict from 12 hrs away to 24 hrs away
-    """
-    def __init__(self, arg):
-        super().__init__()
-
-    def train(self, df, curr_time):
-
-class Ensemble(BaseModel):
-    """
-    Putting everything together
-    """
-    def __init__(self, models):
-        super().__init__()
-        self._models = models
-
-    def train(self, df, curr_time):
-        pass
-
-
+    def score(self, y_test):
+        '''return RMSE of predictions'''
+        return mean_squared_error(y_test, self._y_pred)**0.5
 
 
 def main():
@@ -128,27 +125,110 @@ def main():
     # cut off time with missing load data
     df.dropna(inplace=True)
 
-    # get temperatures as proxy for forecasted temps
-    temps = df.loc[:,'temp']
-
     # create lag variables
-    lagged = df.copy()
-    week = df.shift(668)
-    lagged['last_week'] = week['load'].values
-    day = df.shift(96)
-    lagged['yesterday'] = day['load'].values
+    df['last_week'] = df['load'].shift(668)
+    df['yesterday'] = df['load'].shift(96)
+    for i in np.arange(1,13):
+        lag_name = 'lag-{}'.format(i)
+        df[lag_name] = df['load'].shift(i)
+    # remove first week that does not have sufficient data for all lag variabless
     df.dropna(inplace=True)
 
+    # create time variables
+    df['dow'] = df.index.dayofweek
+    df['hour'] = df.index.hour
+
+    # create weekend variable
+    holidays = ['2012-11-22', '2012-11-23',
+                '2012-12-24', '2012-12-25', '2013-01-01',
+                '2013-05-27', '2013-07-04',
+                '2013-11-28', '2013-11-29']
+    df['wknd'] = np.logical_or.reduce(((df['dow'] == 6).values,
+                    np.logical_and(df['dow']==5, df['hour']>3).values,
+                    np.logical_and(df['dow']==0, df['hour']<8).values,
+                    np.in1d(df.index.strftime("%Y-%m-%d"), holidays))).astype(int)
+
     # create models
+    weekday_model = WeekdayModel()
+    weekend_model = WeekendModel(5)
 
-
+    # stdout logging
+    saveout = sys.stdout
+    fsock = open('out.log', 'w')
+    sys.stdout = fsock
     # list of timestamps to 'go through time'
     times = df.index.tolist()
-    # data starts at 2012-11-02 07:15:00
-    # start forecasting from 2012-11-09 07:15:00
-    for t in times[668:]:
-
+    # iniitialize values
+    last_trained = times[672]
+    train = df.loc[:last_trained]
+    train_weekday = train.loc[df['wknd'] == 0]
+    train_weekend = train.loc[df['wknd'] == 1]
+    weekday_model.train(train_weekday)
+    weekend_model.train(train_weekend)
+    test_errors = []
+    train_errors = []
+    for t in times[673:-3]:
+        # predict 24 hours into future - using actual temp in lieu of forecasted
+        test = df.loc[t+np.timedelta64(15, 'm'):t+np.timedelta64(24, 'h')]
+        y_test = test.pop('load')
+        # pop weekend mask values
+        wkday_mask = test.pop('wknd').values
+        wkend_mask = 1-wkday_mask
+        ## PREDICT only weekday
+        if (wkday_mask == 0).all():
+            weekday_model.predict(test)
+            test_errors.append(weekday_model.score(y_test))
+        ## PREDICT only weekends
+        elif (wkday_mask == 1).all():
+            weekend_model.predict(test)
+            test_errors.append(weekend_model.score(y_test))
+        ## PREDICT using both
+        else:
+            wkday_pred = ma.array(weekday_model.predict(test), mask=wkday_mask)
+            wkend_pred = ma.array(weekend_model.predict(test), mask=wkend_mask)
+            y_pred = wkday_pred + wkend_pred
+            test_errors.append(mean_squared_error(y_test, y_pred.data)**0.5)
+        '''retrain afer 24 hours'''
+        if (t - last_trained) == np.timedelta64(1, 'D'):
+            print(t.strftime("%a, %b %d, %Y"))
+            start = last_trained - np.timedelta64(8, 'D')
+            # retrain weekday model
+            if df.loc[t, 'wknd'] == 0:
+                train = df.loc[start:t]
+                train_weekday = train.loc[df['wknd'] == 0]
+                train_errors.append(weekday_model.train(train_weekday))
+            # retrain weekend model
+            else:
+                train = df.loc[start:t]
+                train_weekend = train.loc[df['wknd'] == 1]
+                train_errors.append(weekend_model.train(train_weekend))
+            last_trained = t
+    # finish logging
+    sys.stdout = saveout
+    fsock.close()
+    '''SOME RESULT PLOTS'''
+    plt.style.use('ggplot')
+    dates = pd.date_range('2012-11-16', periods=len(train_errors), freq='D')
+    trainerrdf = pd.DataFrame(train_errors, dates, ['training'])
+    times = pd.date_range('2012-11-16', periods=len(test_errors), freq='15Min')
+    testerrdf = pd.DataFrame(test_errors, times, ['testing'])
+    errdf = testerrdf.join(trainerrdf)
+    errdf.fillna(method='ffill', inplace=True)
+    errdf['dow'] = errdf.index.dayofweek
+    months = ['2012-11', '2012-12']
+    months.extend(['2013-{}'.format(m) for m in range(1,13)])
+    f, axes = plt.subplots(14, sharey=True)
+    for i, ax in enumerate(axes):
+        current = errdf.loc[months[i]]
+        current[['testing', 'training']].plot(ax=ax, title=months[i])
+        ax.fill_between(current.index, current['testing'], where = current['dow'] > 4,
+                    facecolor = 'red', alpha = 0.5, label='weekend')
+        ax.legend()
+    f.set_size_inches(13, 60)
+    plt.tight_layout()
+    return test_errors, train_errors
 
 
 if __name__ == '__main__':
-    main()
+    pd.options.mode.chained_assignment = None
+    test_errors, train_errors = main()
